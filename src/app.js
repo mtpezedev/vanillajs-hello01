e0ca723fce507e09f584fab0b77d6a34375d01bb
mtpezedev <mtpezequiel@gmail.com>
2025-10-20 06:38:44 +0000

    Proyecto generador de excusas con cajita tipo card con box-shadow

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
+ //* no sabía si borrar esto, no lo estoy usando porque borre el rigo pero... Bueno ahí lo dejo
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+//*           0            1             2            3
+let who   = ['El perro', 'Mi abuela', 'El cartero', 'Mi paloma'];
+let action= ['se comió', 'se meó', 'rompió', 'se cagó'];
+let what  = ['mi tarea', 'mi móvil', 'el carro'];
+let when  = ['antes de clases', 'mientras dormía', 'cuando estaba en el gym', 'mientras almorzaba', 'cuando estaba rezando'];
+
+function getRandom(anyArray) {
+  const index = Math.floor(Math.random() * anyArray.length);
+  return anyArray[index];
+}
+
+window.onload = function () {
+  console.log("Salvado por la campana");
+  const excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`;
+  document.querySelector('#excuse').textContent = excuse; // <p id="excuse">…</p>
 };
+
+
